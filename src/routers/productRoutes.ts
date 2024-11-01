1c2aed18ebb07451439f527073c5b49054bf0d7f
Bsplay14 <brianstiven-14@hotmail.com>
2024-11-01 11:06:11 -0500

    Rutas del producto

--- src/routers/productRoutes.ts
+++ src/routers/productRoutes.ts
@@ -11,7 +11,7 @@ const router = Router();
 
 router.get("products/", getAllProducts); // Trae todos los productos
 router.get("products/:id", getProductByID); // Trae un solo producto
-router.get("products/", createProduct); // Crear un producto
+router.post("products/", createProduct); // Crear un producto
 router.put("products/:id", updateProduct); // Actualizar un producto
 router.delete("products/:id", deleteProduct); // Borrar producto
 
